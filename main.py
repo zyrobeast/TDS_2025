d4312467af506fb7716f5dca5d9a3cf4e669947d
zyrobeast <hgniskesihba@gmail.com>
2025-10-07 11:58:10 +0530

    change output format

--- main.py
+++ main.py
@@ -15,22 +15,26 @@ app.add_middleware(
     expose_headers=["Access-Control-Allow-Origin"]
 )
 
+
 class Query(BaseModel):
     regions: List[str]
     threshold_ms: float
+
 
 @app.post('/')
 async def get_region_metrics(q: Query = Body(...)):
     df = pd.read_json('q-vercel-latency.json')
     df_grouped = df.groupby('region')
-    return {
-        region: {
+    return [
+        {
+            'region': region,
             'avg_latency': float(df_grouped.get_group(region)['latency_ms'].mean()),
             'p95_latency': float(df_grouped.get_group(region)['latency_ms'].quantile(0.95)),
             'avg_uptime': float(df_grouped.get_group(region)['uptime_pct'].mean()),
             'breaches': float(((df['region'] == region) & (df['latency_ms'] > q.threshold_ms)).sum())
         } for region in q.regions
-    }
+    ]
+
 
 if __name__ == '__main__':
     uvicorn.run(app)
