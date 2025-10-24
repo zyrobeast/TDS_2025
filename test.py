2185ed5466860873c42f14d38c78988c7c5cd6b9
zyrobeast <hgniskesihba@gmail.com>
2025-10-24 11:58:27 +0000

    Added Scheduled Workflow

--- test.py
+++ test.py
@@ -15,9 +15,48 @@ import pandas as pd
 # print({'students': df.to_dict(orient='records')})
 
 
-import wikipedia as wk
-page = wk.WikipediaPage('India')
+# import wikipedia as wk
+# page = wk.WikipediaPage('India')
+#
+# from bs4 import BeautifulSoup
+# soup = BeautifulSoup(page.html(), 'html5lib')
+# print(soup.find_all(['h1', 'h2', 'h3', 'h4', 'h5', 'h6']))
 
-from bs4 import BeautifulSoup
-soup = BeautifulSoup(page.html(), 'html5lib')
-print(soup.find_all(['h1', 'h2', 'h3', 'h4', 'h5', 'h6']))
+import requests
+from urllib.parse import urlencode
+
+base_url = "https://nominatim.openstreetmap.org/search"
+params = {
+    'format': 'json',
+    'limit': 10,
+    'city': 'Jeddah',
+    'country': 'Saudi Arabia'
+}
+#
+# # Encode parameters
+# query_string = urlencode(params)
+#
+# # Combine base URL and encoded parameters
+# full_url = f"{base_url}?{query_string}"
+#
+# print(full_url)
+
+places = requests.get(base_url, params=params)
+print(places.text)
+print(places[0]["boundingbox"][0])
+# osm_type = 'N'
+# osm_id = 3717
+#
+# # Build Nominatim lookup URL
+# url = f"https://nominatim.openstreetmap.org/lookup?osm_ids={osm_type}{osm_id}&format=json"
+#
+# headers = {"User-Agent": "test"}
+#
+# response = requests.get(url, headers=headers)
+# data = response.json()
+#
+# if data:
+#     location = data[0]
+#     print(location['lat'], location['lon'])
+# else:
+#     print("Not found")
